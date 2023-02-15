8e393b660a7fe098866be303eafb75d9a2c68c3c
Luis O <70501251+KiryuuLight@users.noreply.github.com>
2023-02-15 18:55:16 -0500

    Console version almost finished , some functions to handle the game logic are not yet created

--- js/app.js
+++ js/app.js
@@ -1,55 +1,119 @@
 /* Console version */
 
-const gameBoard = (function () {
+function GameBoard() {
     const rows = 3;
     const columns = 3;
     const board = [];
-    const valueFill = '[]';
 
     // A simple way to create a board with rows and columns using arrays.
 
     for (let i = 0; i < rows; i += 1) {
         board[i] = [];
         for (let j = 0; j < columns; j += 1) {
-            board[i][j] = valueFill;
+            board[i][j] = createCell();
         }
     }
 
     const getBoard = () => board;
 
+    const putPiece = (row, column, player) => {
+        const availableCells = board.some(
+            (cell, index) => cell[index].getValue() === ''
+        );
+
+        if (!availableCells) return;
+
+        if (board[row][column] !== '') {
+            board[row][column].addValue(player);
+        }
+
+        return board[1][1].getValue();
+    };
+
+    function printBoard() {
+        const print = board.map((row) => row.map((value) => value.getValue()));
+        console.log(print);
+    }
+
     return {
         getBoard,
+        putPiece,
+        printBoard,
     };
-})();
+}
 
-const fillCell = (row, column, player) => {
-    let value;
-    player === 'One' ? (value = 'X') : (value = 'O');
-
-    const board = gameBoard.getBoard();
-    console.log(board.length);
-    const fillWithValue = () => {
-        if (row || column > board.length - 1) return;
-        board[row][column] = value;
-        console.log(board);
+// Token : 0 for [] , 1 for 'X' , 2 for 'O'
+function createCell() {
+    let value = '';
+    const addValue = (token) => {
+        value = token;
     };
+
+    const getValue = () => value;
     return {
-        fillWithValue,
+        getValue,
+        addValue,
     };
-};
+}
 
-const displayController = (function () {})();
+function GameController(playerOne = 'Mario', playerTwo = 'Luigi') {
+    const board = GameBoard();
 
-// GAME LOGIC
-// In our array we have elements positioned by [x][y] dimensions
-// So we are looking for these specific patterns
-// For row : All [x] values have to be the same , [y] values doesn't matter
-// Example : [0][_] [0][_] [0][_]
+    const players = [
+        {
+            name: playerOne,
+            token: '❌',
+        },
+        {
+            name: playerTwo,
+            token: '⭕',
+        },
+    ];
 
-// For column : All [y] values have to be the same , [x] values doesn't matter
-// Example : [_][2] [_][2] [_][2]
+    let activePlayer = players[0];
 
-// Three in a row pattern ! [Left]
-// [0][2] [1][1] [2][0]
-// Three in a row pattern ! [Right]
-// [0][0] [1][1] [2][2]
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(
+            `Election made , inserting value in row [${row}] , column : [${column}]`
+        );
+        board.putPiece(row, column, getActivePlayer().token);
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    // GAME LOGIC
+
+    // In our array we have elements positioned by [x][y] dimensions
+    // So we are looking for these specific patterns
+    // For row : All [x] values have to be the same , [y] values doesn't matter
+    // Example : [0][_] [0][_] [0][_]
+
+    // For column : All [y] values have to be the same , [x] values doesn't matter
+    // Example : [_][2] [_][2] [_][2]
+
+    // Three in a row pattern ! [Left]
+    // [0][2] [1][1] [2][0]
+    // Three in a row pattern ! [Right]
+    // [0][0] [1][1] [2][2]
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer,
+    };
+}
+
+const game = GameController();
